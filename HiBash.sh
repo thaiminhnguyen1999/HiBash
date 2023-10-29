a65f4d0b5c6ba5a67124efbad3a8f424ce3b612d
Thai Minh Nguyen <109969513+thaiminhnguyen1999@users.noreply.github.com>
2023-10-29 20:12:11 +0700

    Add files via upload

---
+++ HiBash.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+export $(grep -v '^#' .env | xargs)
+
+clear
+
+echo "$BASH_NAME [Version $BASH_VERSION]"
+
+echo "(c) $BASH_AUTHOR. All right reserved. Power by Thai Minh Nguyen"
+
+while true
+do
+    echo "[HiBash@$OSTYPE]"
+    read -p "-> " usr_cmdinput
+
+    case $usr_cmdinput in
+        "exit")
+            echo "Exit $BASH_NAME"
+            break
+            ;;
+        "cmd")
+            echo "Command Prompt is only supported on Windows operating systems"
+            ;;
+        "pwsh"|"powershell")
+            clear
+            echo "Waiting to open Powershell"
+            powershell
+            ;;
+        "clear")
+            clear
+            ;;
+        "help")
+            vim help.txt &
+            ;;
+        "shutdown")
+            read -p "? Enter options (h, r, l, h+, h+-, c): " usr_cmdinputoptions
+            case $usr_cmdinputoptions in
+                "h")
+                    shutdown -h now
+                    ;;
+                "r")
+                    shutdown -r now
+                    ;;
+                "l")
+                    gnome-session-quit --logout --no-prompt
+                    ;;
+                "h+")
+                    read -p "? Enter the shutdown time after command activation (second):  " shutdown_timecount
+                    shutdown -h +$shutdown_timecount
+                    ;;
+                "h+-")
+                    read -p "? Enter the shutdown time after command activation (second):  " shutdown_timecount
+                    read -p "? Enter the shutdown message after command activation:  " shutdown_message
+                    shutdown -h +$shutdown_timecount "$shutdown_message"
+                    ;;
+                "c")
+                    shutdown -c
+                    ;;
+                *)
+                    echo "Invalid option"
+                    ;;
+            esac
+            ;;
+        *)
+            echo "Command not found"
+            ;;
+    esac
+done
